b1374a241c5d8f35744c4af53e4ff4719c550f90
HXDCHLL <85605736+OOOOOORAN@users.noreply.github.com>
2022-10-23 13:38:23 +0800

    Site updated: 2022-10-23 13:38:22

---
+++ js/weather.js
@@ -0,0 +1,1 @@
+WIDGET={CONFIG:{modules:"01234",background:"5",tmpColor:"4A86E8",tmpSize:"16",cityColor:"FF9900",citySize:"16",aqiColor:"4A86E8",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"0px 0px 0px 0px",shadow:"0",language:"auto",fixed:"true",vertical:"center",horizontal:"center",left:"190",top:"19",key:"8b1e0212797442b6a3b712fa6a342f3d"}};
